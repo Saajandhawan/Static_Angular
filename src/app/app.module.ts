500c079946f8cf778f8e47a013641c52fbd0bde1
Saajandhawan <109841216+Saajandhawan@users.noreply.github.com>
2022-11-09 10:00:27 +0530

    deal Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './Component/side-bar/side-bar.component';
 import { MainSectionComponent } from './Component/main-section/main-section.component';
